90ce28fa17b509aebf03af9ccc998975e5f6183e
AngeloDeHeijdeDiazLab02 <angelondeh@gmail.com>
2026-03-18 18:36:17 -0400

    Renamed both main activity java and xml files. Added my name and student id to all java and xml files. Changed homescreen icon. Changed all object ids to have ang at the start of the id's names.

---
+++ AngeloModule8/src/main/java/angelo/deheijde/n7790/DeHeijdeActivity8.java
@@ -0,0 +1,25 @@
+//Angelo De Heijde Diaz - N01727790
+package angelo.deheijde.n7790;
+
+import android.os.Bundle;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class DeHeijdeActivity8 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_deheijde);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.angMain), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+    }
+}
