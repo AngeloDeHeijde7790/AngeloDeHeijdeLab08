42b5a36165d84aecdb65a2bca0bd735bb21a7b46
AngeloDeHeijdeDiazLab02 <angelondeh@gmail.com>
2026-03-20 12:12:55 -0400

    Edited both xml and java files of home fragment to diplay my full name and student id, a defaultt image, and button that shuffles trought 4 images. Added a few images.

--- AngeloModule8/src/main/java/angelo/deheijde/n7790/DeHeijdeActivity8.java
+++ AngeloModule8/src/main/java/angelo/deheijde/n7790/DeHeijdeActivity8.java
@@ -7,7 +7,6 @@ import android.net.Uri;
 import android.os.Bundle;
 import android.view.MenuItem;
 import android.widget.TextView;
-import android.widget.Toast;
 
 import androidx.activity.OnBackPressedCallback;
 import androidx.annotation.NonNull;
@@ -17,13 +16,15 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 import androidx.core.view.GravityCompat;
 import androidx.drawerlayout.widget.DrawerLayout;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 
 import com.google.android.material.navigation.NavigationView;
 
 public class DeHeijdeActivity8 extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
 
     private DrawerLayout drawerLayout;
-    private TextView contentTextView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -35,7 +36,6 @@ public class DeHeijdeActivity8 extends AppCompatActivity implements NavigationVi
 
         drawerLayout = findViewById(R.id.angDrawerLayout);
         NavigationView navigationView = findViewById(R.id.angNavigationView);
-        contentTextView = findViewById(R.id.angTextView);
 
         navigationView.setNavigationItemSelectedListener(this);
 
@@ -45,61 +45,49 @@ public class DeHeijdeActivity8 extends AppCompatActivity implements NavigationVi
         toggle.syncState();
 
         if (savedInstanceState == null) {
+            // 47.a: Home Fragment displayed by default
             navigationView.setCheckedItem(R.id.nav_home);
-            contentTextView.setText(R.string.menu_home);
+            replaceFragment(new HomeFragment());
         }
 
-        // Handle user clicks on Android Back Key
         getOnBackPressedDispatcher().addCallback(this, new OnBackPressedCallback(true) {
             @Override
             public void handleOnBackPressed() {
                 if (drawerLayout.isDrawerOpen(GravityCompat.START)) {
-                    // 43.a: If the sliding window is open, close the window.
                     drawerLayout.closeDrawer(GravityCompat.START);
                 } else {
-                    // 43.b: If sliding window already closed, display AlertDialog
                     showExitAlertDialog();
                 }
             }
         });
     }
 
-    // 43.b.i-vi & 44: AlertDialog implementation
     private void showExitAlertDialog() {
         AlertDialog.Builder builder = new AlertDialog.Builder(this);
-        builder.setIcon(R.mipmap.ic_launcher_round); // 43.b.i: Icon (not default)
-        builder.setTitle("Angelo De Heijde Diaz"); // 43.b.ii: Title (Full Name)
-        builder.setMessage("Do you want to exit the app ?"); // 43.b.iii: Message
-        
-        // 43.b.iv: Yes action
+        builder.setIcon(R.mipmap.ic_launcher_round);
+        builder.setTitle("Angelo De Heijde Diaz");
+        builder.setMessage("Do you want to exit the app ?");
         builder.setPositiveButton("Yes", new DialogInterface.OnClickListener() {
             @Override
             public void onClick(DialogInterface dialog, int which) {
-                // Launch YouTube app with a specific video
                 Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse("vnd.youtube:dQw4w9WgXcQ"));
                 intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
                 try {
                     startActivity(intent);
                 } catch (Exception e) {
-                    // Fallback to web browser if YouTube app is not installed
                     intent = new Intent(Intent.ACTION_VIEW, Uri.parse("https://www.youtube.com/watch?v=dQw4w9WgXcQ"));
                     startActivity(intent);
                 }
-                finish(); // Optional: Close the app after launching YouTube
+                finish();
             }
         });
-
-        // 43.b.v: No action
         builder.setNegativeButton("No", new DialogInterface.OnClickListener() {
             @Override
             public void onClick(DialogInterface dialog, int which) {
-                dialog.dismiss(); // Stays in the app
+                dialog.dismiss();
             }
         });
-
-        // 43.b.vi: User must not be able to dismiss without answering Yes or No
         builder.setCancelable(false);
-
         builder.create().show();
     }
 
@@ -108,19 +96,22 @@ public class DeHeijdeActivity8 extends AppCompatActivity implements NavigationVi
         int id = item.getItemId();
 
         if (id == R.id.nav_home) {
-            contentTextView.setText(R.string.menu_home);
-        } else if (id == R.id.nav_settings) {
-            contentTextView.setText(R.string.menu_settings);
-        } else if (id == R.id.nav_share) {
-            contentTextView.setText(R.string.menu_share);
-        } else if (id == R.id.nav_about) {
-            contentTextView.setText(R.string.menu_about);
+            replaceFragment(new HomeFragment());
         } else if (id == R.id.nav_logout) {
-            // 44: If user clicks Logout, display the same AlertDialog
             showExitAlertDialog();
+        } else {
+            // Placeholder for other fragments (Settings, Share, About)
+            // You can create fragments for these as well
         }
 
         drawerLayout.closeDrawer(GravityCompat.START);
         return true;
+    }
+
+    private void replaceFragment(Fragment fragment) {
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+        fragmentTransaction.replace(R.id.angFragmentContainer, fragment);
+        fragmentTransaction.commit();
     }
 }
--- AngeloModule8/src/main/java/angelo/deheijde/n7790/HomeFragment.java
+++ AngeloModule8/src/main/java/angelo/deheijde/n7790/HomeFragment.java
@@ -1,64 +1,61 @@
 // Angelo Noel De Heijde Diaz - N01727790
 package angelo.deheijde.n7790;
+
 import android.os.Bundle;
-
-import androidx.fragment.app.Fragment;
-
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.ImageView;
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link HomeFragment#newInstance} factory method to
- * create an instance of this fragment.
- */
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+import com.google.android.material.snackbar.Snackbar;
+
 public class HomeFragment extends Fragment {
 
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
-
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
+    private int clickCount = 0;
+    private int[] drawables = {
+            R.drawable.emperor,
+            R.drawable.galaxia3,
+            R.drawable.galaxia4,
+            R.drawable.warhammer
+    };
 
     public HomeFragment() {
         // Required empty public constructor
     }
 
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment HomeFragment.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static HomeFragment newInstance(String param1, String param2) {
-        HomeFragment fragment = new HomeFragment();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
-    }
-
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
-    }
-
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
+        // 47.a: Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_home, container, false);
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        ImageView imageView = view.findViewById(R.id.angHomeImageView);
+        Button button = view.findViewById(R.id.angHomeButton);
+
+        // 47.f: Handle button clicks
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                clickCount++;
+
+                // 47.f.i: Rotate between 4 different images
+                int imageIndex = (clickCount - 1) % drawables.length;
+                imageView.setImageResource(drawables[imageIndex]);
+
+                // 47.f.ii: Display a snackbar with short duration
+                String message = "Angelo De Heijde Diaz - " + clickCount;
+                Snackbar.make(view, message, Snackbar.LENGTH_SHORT).show();
+            }
+        });
     }
 }
