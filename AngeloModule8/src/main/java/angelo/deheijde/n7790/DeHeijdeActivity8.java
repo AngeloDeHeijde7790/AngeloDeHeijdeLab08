f3c238c714c9632174c0b56966c58cae3cbecaab
AngeloDeHeijdeDiazLab02 <angelondeh@gmail.com>
2026-03-20 11:30:03 -0400

    Create both java and xml file for DeHeijdeActivity8, a menu folder with its respective xml file. Created multiple icons for the navigation menu to match the colors of the menu header and change its color when selected by using the nav_item_color_state.xml.

--- AngeloModule8/src/main/java/angelo/deheijde/n7790/DeHeijdeActivity8.java
+++ AngeloModule8/src/main/java/angelo/deheijde/n7790/DeHeijdeActivity8.java
@@ -2,24 +2,82 @@
 package angelo.deheijde.n7790;
 
 import android.os.Bundle;
+import android.view.MenuItem;
+import android.widget.TextView;
+import android.widget.Toast;
 
-import androidx.activity.EdgeToEdge;
+import androidx.activity.OnBackPressedCallback;
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
+import androidx.appcompat.widget.Toolbar;
+import androidx.core.view.GravityCompat;
+import androidx.drawerlayout.widget.DrawerLayout;
 
-public class DeHeijdeActivity8 extends AppCompatActivity {
+import com.google.android.material.navigation.NavigationView;
+
+public class DeHeijdeActivity8 extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
+
+    private DrawerLayout drawerLayout;
+    private TextView contentTextView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
         setContentView(R.layout.activity_deheijde);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.angMain), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
+
+        Toolbar toolbar = findViewById(R.id.angToolbar);
+        setSupportActionBar(toolbar);
+
+        drawerLayout = findViewById(R.id.angDrawerLayout);
+        NavigationView navigationView = findViewById(R.id.angNavigationView);
+        contentTextView = findViewById(R.id.angTextView);
+
+        navigationView.setNavigationItemSelectedListener(this);
+
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(this, drawerLayout, toolbar,
+                R.string.navigation_drawer_open, R.string.navigation_drawer_close);
+        drawerLayout.addDrawerListener(toggle);
+        toggle.syncState();
+
+        if (savedInstanceState == null) {
+            navigationView.setCheckedItem(R.id.nav_home);
+            contentTextView.setText(R.string.menu_home);
+        }
+
+        // Fix for deprecated onBackPressed()
+        getOnBackPressedDispatcher().addCallback(this, new OnBackPressedCallback(true) {
+            @Override
+            public void handleOnBackPressed() {
+                if (drawerLayout.isDrawerOpen(GravityCompat.START)) {
+                    drawerLayout.closeDrawer(GravityCompat.START);
+                } else {
+                    setEnabled(false);
+                    getOnBackPressedDispatcher().onBackPressed();
+                    setEnabled(true);
+                }
+            }
         });
+    }
+
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+        int id = item.getItemId();
+
+        if (id == R.id.nav_home) {
+            contentTextView.setText(R.string.menu_home);
+        } else if (id == R.id.nav_settings) {
+            contentTextView.setText(R.string.menu_settings);
+        } else if (id == R.id.nav_share) {
+            contentTextView.setText(R.string.menu_share);
+        } else if (id == R.id.nav_about) {
+            contentTextView.setText(R.string.menu_about);
+        } else if (id == R.id.nav_logout) {
+            Toast.makeText(this, "Logging out...", Toast.LENGTH_SHORT).show();
+            contentTextView.setText(R.string.menu_logout);
+        }
+
+        drawerLayout.closeDrawer(GravityCompat.START);
+        return true;
     }
 }
